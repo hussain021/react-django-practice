1a2c4597b32151ba7e8bfbf27dac74d9ad1f2ba3
Hussain Zaidi <hussain.zaidi@arbisoft.com>
2021-09-02 01:54:19 +0500

    using _(underscore) prefix for internal use variables

--- clothes_spider/spiders/sheego_spider.py
+++ clothes_spider/spiders/sheego_spider.py
@@ -14,7 +14,7 @@ class SheegoSpider(scrapy.Spider):
         for product in response.css(
             'section.cj-product.js-product-box.js-product-box--list.js-unveil-plbox.at-product-box'
             ):
-            single_product_url = self.get_single_product_url(product)
+            single_product_url = self._get_single_product_url(product)
             yield scrapy.Request(
                 url='https://sheego.de/{product_url}'.format(product_url=single_product_url),
                 callback=self.parse_product
@@ -27,42 +27,42 @@ class SheegoSpider(scrapy.Spider):
             folder.
         """
         yield ProductItem(
-            title = self._clean([self.get_title(response)])[0],
-            price = eval([size.replace(',','.') for size in [self.get_price(response)[:-2]]][0]), 
-            currency = self._clean([self.get_price(response)[-2:]])[0],
-            image_urls = self.format_image_urls(self.get_image_urls(response)),
-            all_sizes = [eval(size) for size in self._clean(self.get_all_sizes(response))],
-            all_colors = self._clean(self.get_all_colors(response)),
-            description = self._clean([self.get_description(response)])[0],
+            title = self._clean([self._get_title(response)])[0],
+            price = eval([size.replace(',','.') for size in [self._get_price(response)[:-2]]][0]), 
+            currency = self._clean([self._get_price(response)[-2:]])[0],
+            image_urls = self._format_image_urls(self._get_image_urls(response)),
+            all_sizes = [eval(size) for size in self._clean(self._get_all_sizes(response))],
+            all_colors = self._clean(self._get_all_colors(response)),
+            description = self._clean([self._get_description(response)])[0],
         )
 
-    def get_single_product_url(self, product):
+    def _get_single_product_url(self, product):
         return product.css('a.js-product__link::attr(href)').extract_first()
 
-    def get_title(self, response):
+    def _get_title(self, response):
         return response.css('span.p-details__name.l-text-1.at-name::text').extract_first()
     
-    def get_price(self, response):
+    def _get_price(self, response):
         return response.css('span.l-subline-2.l-bold.product__price__current.at-lastprice::text').extract_first()             
 
-    def get_all_sizes(self, response):
+    def _get_all_sizes(self, response):
         return response.css('div.sizespots__item.js-click-variant.at-dv-size-button::text').extract()
 
-    def get_image_urls(self, response):
+    def _get_image_urls(self, response):
         return response.css('span.l-pointer::attr(data-image)').extract()
 
-    def get_all_colors(self, response):
+    def _get_all_colors(self, response):
         return response.css(
                 'div.cj-slider__item.js-click-variant.at-dv-color.colorspots__item > img.js-slider__unveil::attr(alt)'
             ).extract()
 
-    def get_description(self, response):
+    def _get_description(self, response):
         return response.css('div.details__box__desc.at-dv-artDes.l-pr-10 > p::text').extract_first()
 
     def _clean(self, string_list:list):
         return list(map((str.strip), string_list))
 
-    def format_image_urls(self, image_urls):
+    def _format_image_urls(self, image_urls):
         return [    
             'https:{image_url}'.format(
                 image_url=image_urls[index]
