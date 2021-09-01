060b303e7400afc022f240560c622aebf90371cf
Hussain Zaidi <hussain.zaidi@arbisoft.com>
2021-09-02 01:41:16 +0500

    Single generic clean function

--- clothes_spider/spiders/sheego_spider.py
+++ clothes_spider/spiders/sheego_spider.py
@@ -18,7 +18,7 @@ class SheegoSpider(scrapy.Spider):
             yield scrapy.Request(
                 url='https://sheego.de/{product_url}'.format(product_url=single_product_url),
                 callback=self.parse_product
-                )
+            )
 
     def parse_product(self, response):
         """This function scraps the detail of each product using the url that has been passed to it
@@ -26,23 +26,15 @@ class SheegoSpider(scrapy.Spider):
             this function yields the data in form of ProductItem and stores the images in the images
             folder.
         """
-        product_details = {
-            'title': self.get_title(response),
-            'price': self.get_price(response),
-            'all_sizes': self.get_all_sizes(response),
-            'image_urls': self.get_image_urls(response),
-            'all_colors': self.get_all_colors(response),
-            'description': self.get_description(response),
-        }
         yield ProductItem(
-            title = self.cleaning([product_details['title']])[0],
-            price = self.cleaning_and_casting([product_details['price'][:-2]])[0], 
-            currency = self.cleaning([product_details['price'][-2:]])[0],
-            image_urls = self.format_image_urls(product_details['image_urls']),
-            all_sizes = self.cleaning_and_casting(product_details['all_sizes']),
-            all_colors = self.cleaning(product_details['all_colors']),
-            description = self.cleaning([product_details['description']])[0],
-            )
+            title = self._clean([self.get_title(response)])[0],
+            price = eval([size.replace(',','.') for size in [self.get_price(response)[:-2]]][0]), 
+            currency = self._clean([self.get_price(response)[-2:]])[0],
+            image_urls = self.format_image_urls(self.get_image_urls(response)),
+            all_sizes = [eval(size) for size in self._clean(self.get_all_sizes(response))],
+            all_colors = self._clean(self.get_all_colors(response)),
+            description = self._clean([self.get_description(response)])[0],
+        )
 
     def get_single_product_url(self, product):
         return product.css('a.js-product__link::attr(href)').extract_first()
@@ -62,24 +54,19 @@ class SheegoSpider(scrapy.Spider):
     def get_all_colors(self, response):
         return response.css(
                 'div.cj-slider__item.js-click-variant.at-dv-color.colorspots__item > img.js-slider__unveil::attr(alt)'
-                ).extract()
+            ).extract()
 
     def get_description(self, response):
         return response.css('div.details__box__desc.at-dv-artDes.l-pr-10 > p::text').extract_first()
 
-    def cleaning_and_casting(self, numeric_list):
-        numeric_list=[size.replace(',','.') for size in numeric_list]
-        return [
-            eval(x) for x in list(map((str.strip), numeric_list))
-            ]
-
-    def cleaning(self, string_list:list):
+    def _clean(self, string_list:list):
         return list(map((str.strip), string_list))
 
     def format_image_urls(self, image_urls):
         return [    
             'https:{image_url}'.format(
-            image_url=image_urls[index])
+                image_url=image_urls[index]
+                )
             for index, url in enumerate(image_urls)
             ]
             
